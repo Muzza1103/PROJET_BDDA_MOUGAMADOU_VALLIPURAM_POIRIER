07a5811095547b98aabaa08f5b2734956f000386
seraphinpoirier <poirier.seraphin@gmail.com>
2023-09-28 16:00:27 +0200

    tests_dbparams

---
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
@@ -0,0 +1,11 @@
+public class DBParams {
+
+    public static String DBPath;
+    public static long SGBDPageSize;
+    public static int DMFileCount;
+
+    public static void main (String[] args){
+        System.out.println("test");
+    }
+
+}

49bc94d30b1c6f55695e88168e6e3e74f8353f15
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-10-05 00:21:02 +0200

    Update PageId.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/PageId.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/PageId.java
@@ -1,6 +1,19 @@
 
 public class PageId {
-	private int FileIdx;
-	private int PageIdx;
-}
+	private int fileIdx;
+	private int pageIdx;
 
+
+	public PageId(int fileIdx, int pageIdx){
+		this.fileIdx = fileIdx;
+		this.pageIdx = pageIdx;
+	}
+	
+	public int getFileIdx() {
+		return fileIdx;
+	}
+
+	public int getPageIdx() {
+		return pageIdx;
+	}
+}

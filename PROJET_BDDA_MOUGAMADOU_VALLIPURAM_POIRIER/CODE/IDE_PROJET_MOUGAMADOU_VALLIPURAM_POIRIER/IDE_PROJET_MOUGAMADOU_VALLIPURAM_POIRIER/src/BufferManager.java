bdaad0959295fc15cb9fad17d2e9ddc2d1ca26c9
seraphinpoirier <poirier.seraphin@gmail.com>
2023-10-05 13:42:13 +0200

    creation classe buffermanager

---
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/BufferManager.java
@@ -0,0 +1,14 @@
+public class BufferManager {
+    public buff GetPage(PageId ID_page){
+
+    }
+
+    public void FreePage (PageId ID_page, boolean valdirty){
+
+    }
+
+    public void FlushAll(){
+        
+    }
+
+}

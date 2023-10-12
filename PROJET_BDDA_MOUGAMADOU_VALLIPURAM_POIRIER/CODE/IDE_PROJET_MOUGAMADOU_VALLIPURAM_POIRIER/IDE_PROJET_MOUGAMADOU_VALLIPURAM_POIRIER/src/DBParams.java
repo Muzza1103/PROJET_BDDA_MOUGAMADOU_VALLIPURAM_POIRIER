c78249ce561208bcf9e85f32631894ba8003fbc5
VALLIPURAM-MANIKAM THARSHIKAN <in07091@migre-6.ens.math-info.univ-paris5.fr>
2023-10-12 16:28:44 +0200

    getPAge()

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
@@ -3,12 +3,14 @@ public class DBParams {
     public static String DBPath;
     public static long SGBDPageSize;
     public static int DMFileCount;
+    public static int frameCount;
 
     public static void main (String[] args){
         System.out.println("test");
         DBPath = "/users/licence/in07091/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/DB/";
         SGBDPageSize = 4096;
         DMFileCount = 4;
+        frameCount = 2;
 
     }
 
--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DiskManagerTests.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DiskManagerTests.java
@@ -5,11 +5,11 @@ public class DiskManagerTests {
 	public static void main(String [] args) {
 		DBParams.DBPath = "/users/licence/in07091/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/DB/";
 		DBParams.SGBDPageSize = 4096;
-        	DBParams.DMFileCount = 4;
+        DBParams.DMFileCount = 4;
 		
-		//TestAllocationPage();
+		TestAllocationPage();
 
-		//TestDesAllocPage();
+		//TestDesAllocPage();3
 
 		TestEcritureLecturePage();
 	}

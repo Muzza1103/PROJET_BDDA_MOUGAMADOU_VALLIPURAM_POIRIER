7dd2da9671cb9f89afa78232d65ed2016f1d4a0c
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-10-12 16:33:15 +0200

    Update BufferManager.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/BufferManager.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/BufferManager.java
@@ -1,11 +1,57 @@
 import java.nio.ByteBuffer;
+import java.util.*;
 
-public class BufferManager {
+public final class BufferManager {
+	
+	private static BufferManager bm = null;
+	private List<Frame> listFrames;
+	
+	
+	private BufferManager() {
+		listFrames = Arrays.asList(new Frame[DBParams.frameCount]);
+	}
+	
+	public static BufferManager getInstance() {
+		if (bm == null) {
+			bm = new BufferManager();
+		}
+		return bm;
+	}
+	
     public ByteBuffer GetPage(PageId ID_page){
+    	ByteBuffer byteB = ByteBuffer.allocate((int)DBParams.SGBDPageSize);
+    	List<Frame> listFrameZero = Arrays.asList(new Frame[DBParams.frameCount]);
+    	int cptNbreAccees=0;
+    	for(Frame f : listFrames) {
+    		if(f.getPageId() == ID_page) {
+    			f.incrPinCount();
+    			f.getPageId().incrNbreAcces();
+    			return f.getByteBuffer();
+    		}
+    	}
+    	for(int i = 0; i < listFrames.size(); i++) {
+    		if(listFrames.get(i).getPageId() == null && listFrames.get(i).getByteBuffer() == null ) {
+    			listFrames.get(i).incrPinCount();
+    			listFrames.get(i).getPageId().incrNbreAcces();
+    			return listFrames.get(i).getByteBuffer();
+    		}
+    		else if(listFrames.get(i).getPinCount() == 0 ) {
+    			listFrameZero.add(listFrames.get(i));
+    		}	
+    	}
+    	if (!listFrameZero.isEmpty()) {
+    		for (int i=0;i<listFrameZero.size();i++) {
+    			if (listFrameZero.get(i).getPageId().getNbreAcces()<listFrameZero.get(i+1).getPageId().getNbreAcces()) {
+    				cptNbreAccees = i;
+    			}
+    		}
+    		return listFrameZero.get(cptNbreAccees).getByteBuffer();
+    	}
     	
-
+    	return byteB;
     }
-
+    
+    
     public void FreePage (PageId ID_page, int valdirty){
         
     }

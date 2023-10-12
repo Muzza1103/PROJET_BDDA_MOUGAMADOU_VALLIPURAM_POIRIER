0685efb2b9a0c78a2cf7fd0da0cc23e1d9a10062
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-10-12 16:33:50 +0200

    Update Frame.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Frame.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Frame.java
@@ -1,14 +1,28 @@
 import java.nio.ByteBuffer;
 
 public class Frame {
-    public int pin_count;
-    public int valdirty;
-    public PageId page_chargee;
-    public ByteBuffer buffer;
+    private int pin_count;
+    private int valdirty;
+    private PageId page_chargee;
+    private ByteBuffer buffer;
     
     public void initFrame(){
         this.pin_count = 0;
         this.valdirty = 0;
+    }
+    
+    public PageId getPageId() {
+    	return page_chargee;
+    }
+    
+    public ByteBuffer getByteBuffer() {
+    	return buffer;
+    }
+    public int getPinCount() {
+    	return pin_count;
+    }
+    public int getValDirty() {
+    	return valdirty;
     }
 
     public void updateValdirty(int new_value){
@@ -18,9 +32,14 @@ public class Frame {
     public void resetPin_count(){
         this.pin_count = 0;
     }
+    
+    public void incrPinCount() {
+    	this.pin_count ++;
+    }
 
     public void decreasePin_count(){
         this.pin_count --;
     }
 
 }
+

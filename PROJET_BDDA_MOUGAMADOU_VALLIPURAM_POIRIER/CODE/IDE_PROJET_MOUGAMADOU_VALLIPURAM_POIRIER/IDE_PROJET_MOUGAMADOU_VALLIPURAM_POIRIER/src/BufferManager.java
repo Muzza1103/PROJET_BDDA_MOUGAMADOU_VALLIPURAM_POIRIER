eda6edd5e3a78fbc8e396aa1058dabf1919cbf9e
seraphinpoirier <poirier.seraphin@gmail.com>
2023-10-12 14:35:02 +0200

    debut buffermanager

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/BufferManager.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/BufferManager.java
@@ -1,17 +1,30 @@
 import java.nio.ByteBuffer;
-
+import java.util.*;
+    
 public class BufferManager {
+    List<Frame> list_frames = Arrays.asList(new Frame[DBParams.FrameCount]);
+    
     public ByteBuffer GetPage(PageId ID_page){
-    	
+        
 
     }
 
-    public void FreePage (PageId ID_page, int valdirty){
-        
+    public void FreePage (PageId ID_page, int new_valdirty){
+        for (Frame frame : list_frames) {
+            if(frame.page_chargee == ID_page){
+                frame.decreasePin_count();
+                frame.valdirty = new_valdirty;
+            }
+        }
     }
 
     public void FlushAll(){
-
+        for (Frame frame : list_frames) {
+            if(frame.valdirty == 1){
+                //ECRITURE
+            }
+            frame.valdirty = 0;
+        }
     }
 
 }
--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
@@ -3,12 +3,14 @@ public class DBParams {
     public static String DBPath;
     public static long SGBDPageSize;
     public static int DMFileCount;
+    public static int FrameCount;
 
     public static void main (String[] args){
         System.out.println("test");
         DBPath = "/users/licence/in07091/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/DB/";
         SGBDPageSize = 4096;
         DMFileCount = 4;
+        FrameCount = 2;
 
     }
 

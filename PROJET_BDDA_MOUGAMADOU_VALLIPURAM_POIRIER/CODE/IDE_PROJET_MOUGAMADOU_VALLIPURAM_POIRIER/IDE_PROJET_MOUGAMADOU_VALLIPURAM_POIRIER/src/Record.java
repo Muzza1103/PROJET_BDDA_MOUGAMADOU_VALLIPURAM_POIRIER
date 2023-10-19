b9140150eb458cf9146bf9053fd29af8efb70646
seraphinpoirier <poirier.seraphin@gmail.com>
2023-10-19 14:51:04 +0200

    record taille fixe

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Record.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Record.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class Record {
     private static TableInfo tabInfo;
     private static ArrayList recvalues;
+    //CONSTANTE T
+    private static int T;
 
     public Record(TableInfo tabInfo){
         this.tabInfo = tabInfo;
@@ -11,17 +13,54 @@ public class Record {
     }
 
     public int WriteToBuffer(ByteBuffer buffer, int pos){
-        int taille = 0;
+        int taille = pos;
+        /* 
         if(!recvalues.isEmpty()){
             for (Object object : recvalues) {
                 object = null;
             }
-        }
+        }*/
         
-        //Ecrire recvalues dans buffer si pas de varstring
-        if(tabInfo.getColInfo().GetTypCol().contains(STRING(T)) || tabInfo.getColInfo().GetTypCol().contains(VARSTRING(T))){
-            //on utilise le modèle 
+        //extraire la liste des types dans la table
+        ArrayList<String> types_contenus;
+        for (int j=0; j<tabInfo.getColInfoList().size(); j++) {
+            types_contenus.add(tabInfo.getColInfo(j).GetTypCol());
         }
+
+        //Ecrire recvalues dans buffer si pas de varstring
+        if(!types_contenus.contains(VARSTRING(T))){
+            //on utilise le modèle taille fixe
+            for(int i=0; i<recvalues.size(); i++){
+                //on vérifie le type de la relation
+                //gestion pour STRING
+                if(tabInfo.getColInfo(i).GetTypCol() == STRING(T)){
+                    
+                    buffer.position(pos);
+
+                    buffer.put((byte)recvalues.get(i));
+
+                    pos += T;
+
+                } 
+                //gestion pour float & int
+                else {
+                    buffer.position(pos);
+                    
+                    if(tabInfo.getColInfo(i).GetTypCol == FLOAT()){
+                        float inter_float = (float) recvalues.get(i);
+                        buffer.put((byte)inter_float);
+                    } 
+                    
+                    else if (tabInfo.getColInfo(i).GetTypCol() == INT()){
+                        int inter_int = (int) recvalues.get(i);
+                        buffer.put((byte)inter_int);
+                    }
+                    pos += T;
+                }
+            }
+        }
+
+        taille = T-pos;
         return taille;
     }
 }
--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/TableInfo.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/TableInfo.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class TableInfo {
     private static String nom_rel;
     private static int nb_colonnes;
-    private static ColInfo col_info;
+    private static ArrayList <ColInfo> col_info;
 
     public TableInfo(){
 
@@ -11,7 +13,11 @@ public class TableInfo {
         return this.nom_rel;
     }
 
-    public ColInfo getColInfo(){
+    public ColInfo getColInfo(int i){
+        return this.col_info.get(i);
+    }
+
+    public ArrayList<ColInfo> getColInfoList(){
         return this.col_info;
     }
 

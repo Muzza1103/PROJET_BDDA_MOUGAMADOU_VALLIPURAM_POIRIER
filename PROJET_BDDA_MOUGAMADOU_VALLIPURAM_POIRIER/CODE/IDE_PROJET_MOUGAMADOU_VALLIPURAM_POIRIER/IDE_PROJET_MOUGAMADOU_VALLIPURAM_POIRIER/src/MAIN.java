6efd108e59bbf0bc6ce50a2f64ca11b08032e641
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-11-23 16:38:06 +0100

    Update MAIN.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/MAIN.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/MAIN.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
 
-public class MAIN {
+public class Main {
 	public static void main(String [] args) {
-		System.out.println("Coucou !");
+		DBParams.DBPath = "/users/licence/in07091/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/DB/";
+		DBParams.SGBDPageSize = 4096;
+        DBParams.DMFileCount = 4;
+        
+        DiskManager dk = DiskManager.getInstance();
+        BufferManager bm = BufferManager.getInstance();
+        DataBaseInfo dbi = DataBaseInfo.getInstance();
+        DatabaseManager dbm = DatabaseManager.getInstance();
+        
+        dbm.Init();
+        
+        try (Scanner sc = new Scanner(System.in)) {
+        	System.out.println("EXIT");
+        	System.out.println("AUTRE");
+        	String s = sc.next();
+        	if(s.equals("EXIT")) {
+        		dbm.Finish();
+        	}else {
+        		dbm.ProcessCommand(s);
+        	}
+        	
+		}
+        
+		System.out.println("Coucou Chef Picsou!");
+		
 	}
 }

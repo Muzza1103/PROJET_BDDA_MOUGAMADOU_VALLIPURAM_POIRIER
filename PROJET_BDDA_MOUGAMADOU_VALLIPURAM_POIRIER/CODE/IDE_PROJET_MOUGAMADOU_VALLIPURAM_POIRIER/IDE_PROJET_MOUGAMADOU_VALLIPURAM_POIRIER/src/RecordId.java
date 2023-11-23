06a7f5f5ef46573e33e7ca20564c493ca9cc5d69
Thibhan <98709829+Thibhan@users.noreply.github.com>
2023-11-23 15:56:44 +0100

    Add files via upload

---
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/RecordId.java
@@ -0,0 +1,11 @@
+
+public class RecordId {
+	private PageId pageId;
+	private int slotIdx;
+	
+	public RecordId (PageId pageId, int slotIdx) {
+		this.pageId=pageId;
+		this.slotIdx=slotIdx;
+	}
+
+}

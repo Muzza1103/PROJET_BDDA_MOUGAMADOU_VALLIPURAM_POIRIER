3f3efa49c94d00c4513a341d9e7939fa7c565e1c
Thibhan <98709829+Thibhan@users.noreply.github.com>
2023-11-30 15:57:15 +0100

    Update FileManager.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManager.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManager.java
@@ -16,19 +16,59 @@ public class FileManager {
 	
 	public PageId createNewHeaderPage() {
 		PageId pageId = DiskManager.getInstance().AllocPage();
-		BufferManager.getInstance().GetPage(pageId);
-		String page1 = "-1,0";
-		String page2 = "-1,1";
+		ByteBuffer buf = BufferManager.getInstance().GetPage(pageId);
 		// correspond aux pages factice
-		ByteBuffer  buff1 =  ByteBuffer.allocate((int)DBParams.SGBDPageSize); 
-		buff1.put(page1.getBytes());
-		buff1.put(page2.getBytes());
-		DiskManager.getInstance().WritePage(pageId,buff1);
 		
-		
-		
+		buf.putInt(-1);
+		buf.putInt(0);
+		buf.putInt(-1);
+		buf.putInt(0);
 		BufferManager.getInstance().FreePage(pageId,1);
 		return pageId;
+	}
+	
+	
+	public PageId addDataPage(TableInfo tabInfo) {
+		PageId page = DiskManager.getInstance().AllocPage();// correspond a la datapage 
+		ByteBuffer bufpage = BufferManager.getInstance().GetPage(page);
+	    ByteBuffer buf = BufferManager.getInstance().GetPage(tabInfo.getHeaderPageId());
+	    if((buf.getInt(0)==-1 )&&(buf.getInt(1)==0)) {// correspond au cas la liste est vide
+	    	// ecrire dans la headerpage   le pageId de la page data 
+	    	buf.putInt(0,page.getFileIdx());
+	    	buf.putInt(1,page.getPageIdx());
+	    	BufferManager.getInstance().FreePage(tabInfo.getHeaderPageId(), 1);
+	    }else {
+	    	int a =buf.getInt(0);
+	    	int b =buf.getInt(1);
+	    	bufpage.putInt(a);
+	    	bufpage.putInt(b);
+	    	BufferManager.getInstance().FreePage(page, 1);
+		    //Metre à jour dans la headerpage  
+		    buf.putInt(page.getFileIdx());
+		    buf.putInt(page.getPageIdx());
+		    BufferManager.getInstance().FreePage(tabInfo.getHeaderPageId(), 1);
+	    	}
+	    return page;
+	    	
+	    	
+	    	
+	    }
+	    
+	    
+	public PageId getFreeDataPageId(TableInfo tabInfo,int  sizeRecord){
+	ByteBuffer	 buf = BufferManager.getInstance().GetPage(tabInfo.getHeaderPageId());
+	 int a = buf.getInt(0);
+	 int b = buf.getInt(1);
+	 if((a==-1)&&(b==0)){
+		 //page pas existant dans la listelibre
+		 return null;
+	 }
+	 // par courir tous la liste est vérifé l'espace Libre 
+	  
+		 
+	}
+	public Recordid writeRecordToDataPage(Record record ,PageId pageid) {
+		
 	}
 
 }

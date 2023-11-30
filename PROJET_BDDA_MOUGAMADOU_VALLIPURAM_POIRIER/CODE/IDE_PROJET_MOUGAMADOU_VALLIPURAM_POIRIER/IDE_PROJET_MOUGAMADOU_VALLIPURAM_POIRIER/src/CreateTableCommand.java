15c82dfcbffbf0fc8562a3b194cf60ba84d4b2d8
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-11-30 16:42:14 +0100

    Update CreateTableCommand.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/CreateTableCommand.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/CreateTableCommand.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreateTableCommand {
 	
-	private String nom;
+	private String nom; 
 	private int nbreCol;
 	private List<String> nomCol;
-	private List<String> typeCol;
+	private List<TypeColonne> typeCol;
 	
 	
-	public CreateTableCommand(String nom, int nbreCol, List<String> nomCol, List<String> typeCol ) {
+	public CreateTableCommand(String nom, int nbreCol, List<String> nomCol, List<TypeColonne> typeCol ) {
 		this.nom = nom;
 		this.nbreCol = nbreCol;
 		this.nomCol = nomCol;
 		this.typeCol = typeCol;
 	}
 	
+	private ArrayList<ColInfo> getColInfo(){
+		ArrayList <ColInfo> colInfo = new ArrayList<>();
+		for(int i = 0; i < nomCol.size(); i++) {
+			colInfo.add(new ColInfo(nomCol.get(i), typeCol.get(i)));
+		}
+		return colInfo;
+	}
 	
 	public void execute() {
+		FileManager fm = FileManager.getInstance();
+		DataBaseInfo dbi = DataBaseInfo.getInstance();
 		
+		PageId pageId = fm.createNewHeaderPage();
+		TableInfo tableInfo = new TableInfo(nom, nbreCol, pageId);
+		tableInfo.setColInfo(getColInfo());
+		dbi.addTableInfo(tableInfo);
 	}
 	
 	

1e3778651b803de80dace9408cf6478580e0a4d6
seraphinpoirier <poirier.seraphin@gmail.com>
2023-12-07 14:57:15 +0100

    updates

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DBParams.java
@@ -18,10 +18,14 @@ public class DBParams {
 
         ColInfo Colonne0 = new ColInfo("Nom", "VARSTRING(T)");
         ColInfo Colonne1 = new ColInfo("Age", "INT");
+        ColInfo Colonne2 = new ColInfo("Taille", "FLOAT");
+        ColInfo Colonne3 = new ColInfo("Genre", "STRING(T)");
 
         ArrayList<ColInfo> ListeColonnes = new ArrayList<>();
         ListeColonnes.add(Colonne0);
         ListeColonnes.add(Colonne1);
+        ListeColonnes.add(Colonne2);
+        ListeColonnes.add(Colonne3);
 
         TableInfo Table0 = new TableInfo("nom-age", 2, ListeColonnes);
 
@@ -30,9 +34,14 @@ public class DBParams {
         ArrayList<Object> recValues = new ArrayList<>();
         String nom_0 = "Matthieu";
         int age_0 = 2;
+        float taille_0 = 1.07f;
+        String genre_0 = "M";
+
         
         recValues.add(nom_0);
         recValues.add(age_0);
+        recValues.add(taille_0);
+        recValues.add(genre_0);
         System.out.println(recValues);
 
         Record0.InsertValues(recValues);

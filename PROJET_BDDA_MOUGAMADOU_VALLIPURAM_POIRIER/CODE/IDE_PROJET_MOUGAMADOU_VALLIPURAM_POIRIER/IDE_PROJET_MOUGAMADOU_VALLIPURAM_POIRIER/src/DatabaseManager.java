3f8e94bc7b0f6eb30fd46e58a49673fd59077497
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-12-07 17:43:18 +0100

    Update DatabaseManager.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DatabaseManager.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/DatabaseManager.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,51 +34,95 @@ public class DatabaseManager {
 		bm.FlushAll();
 	}
 	
+	public void remiseZero() {
+		DataBaseInfo dbi = DataBaseInfo.getInstance();
+		BufferManager bm = BufferManager.getInstance();
+		dbi.flushAll();
+		bm.FlushAll();
+		
+	}
 	public void ProcessCommand(String commande) {
 
 		String[] mots = commande.split(" ");
+		
+		List<String> nomColonnes = new ArrayList<>();
+		List<TypeColonne> typeColonnes = new ArrayList<>();
+		DataBaseInfo dbi = DataBaseInfo.getInstance();
 		switch (mots[0]){
 
 			case "CREATE":
-					List<String> nomColonne = new ArrayList<>();
-					List<TypeColonne> typeColonne = new ArrayList<>();
-					String ligne = mots[3].substring(1,mots[3].length()-1);
-					String[] mots2 = ligne.split("[:,]");;
-					int nbrColonne;
-					for(int i = 0; i < mots2.length; i+=2 ) {
-						nomColonne.add(mots2[i]);
-						//System.out.println(mots2[i]);							
-						if(mots2[i+1].contains("STRING")||mots2[i+1].contains("VARSTRING")) {
-							System.out.println(mots2[i+1]);
-							int startIndex = mots2[i+1].indexOf("(");
-							int endIndex = mots2[i+1].indexOf(")");
-							//System.out.println(startIndex);
-							//System.out.println(endIndex);
-							String subString = mots2[i+1].substring(startIndex + 1,endIndex);
-							typeColonne.add(new TypeColonne(mots2[i+1],Integer.parseInt(subString)));
-						}else {
-							//System.out.println(mots2[i+1]);
-							typeColonne.add(new TypeColonne(mots2[i+1]));
+				String ligne = mots[3].substring(1,mots[3].length()-1);
+				String[] mots2 = ligne.split("[:,]");
+				int nbrColonne;					
+				for(int i = 0; i < mots2.length; i += 2 ) {
+					nomColonnes.add(mots2[i]);						
+					if( mots2[i+1].contains("STRING") || mots2[i+1].contains("VARSTRING") ) {
+						System.out.println(mots2[i+1]);
+						int startIndex = mots2[i+1].indexOf("(");
+						int endIndex = mots2[i+1].indexOf(")");
+						String subString = mots2[i+1].substring(startIndex + 1,endIndex);
+						typeColonnes.add(new TypeColonne(mots2[i+1],Integer.parseInt(subString)));
+					}else {
+						typeColonnes.add(new TypeColonne(mots2[i+1]));
 					
-						}
-					nbrColonne = nomColonne.size();
-					CreateTableCommand ctc = new CreateTableCommand(mots[2], nbrColonne, nomColonne, typeColonne);
+					}
+					nbrColonne = nomColonnes.size();
+					CreateTableCommand ctc = new CreateTableCommand(mots[2], nbrColonne, nomColonnes, typeColonnes);
 					ctc.execute(); 
 				}
 				break;
 			
 			case "RESETDB":
-				for(int i=0;i<5;i++) {
-					String chemin = DBParams.DBPath+"F"+i+".bin"; 
+				for(int i = 0; i < 5; i++) {
+					String chemin = DBParams.DBPath + "F" + i + ".bin"; 
 					File fichierASupprimer = new File(chemin);
 					if(fichierASupprimer.exists()) {
 						fichierASupprimer.delete();
 					}
 				}
+				remiseZero();
 				break;
 			
 			case "INSERT":
-
+				Record record = new Record(null);
+				List<String> motColonnes = new ArrayList<>();
+				
+				/*
+				 * Ajout du nom de la relation dans la relation
+				 */
+				String nomRelation = mots[2];
+				int numTable = -1;
+				String ligneInsert = mots[4].substring(1, mots[4].length()-1);
+				String[] mots3 = ligneInsert.split("(,)");
+				for(TableInfo ti : dbi.getList()) {
+					if(ti.getNom().equals(nomRelation)) {
+						numTable = dbi.getList().indexOf(ti);
+					}
+				}
+				if (numTable != -1) {
+					Record rec = new Record(dbi.getList().get(numTable));
+					ArrayList<Object> recvalues = new ArrayList<>();
+					for(int i = 0; i < mots3.length; i++) {
+						typ //Changer pour vérifier le type
+						recvalues.add(mots3[i]);
+					}
+					int nbrColloneValide = 0;
+					for (int i=0;i<dbi.getList().get(numTable).getNbColonnes();i++) {
+						if()
+					}
+				}
+				
+				
+				ArrayList<Object> recvalues = new ArrayList<>();
+				for (int i=0;i<)
+				
+				for(int i = 0; i < mots3.length; i++) {
+					motColonnes.add(mots3[i]);
+				}
+				
+				
+				
+				
 				break;
 				
 			case "SELECT":

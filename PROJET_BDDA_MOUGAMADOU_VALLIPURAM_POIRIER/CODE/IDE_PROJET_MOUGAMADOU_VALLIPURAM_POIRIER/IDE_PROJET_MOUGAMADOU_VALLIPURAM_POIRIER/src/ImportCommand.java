2b42815f72cab47fb8d41a28a6ddeece12e9450e
tharsi12 <133108179+tharsi12@users.noreply.github.com>
2023-12-14 15:54:43 +0100

    Update ImportCommand.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/ImportCommand.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/ImportCommand.java
@@ -3,17 +3,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.BufferedReader;
+import java.util.Scanner;
+
 
 /*
  * Loin d'avoir fini mais oklm on a la santé
- * des doutes sur quelques infos de comprehension(1 record par ligne, les autres valeurs servent a quoi?, nomRelation != Record en question?)
  */
 
 public class ImportCommand{
 	private Record record;
 	private String nomRelation;
-	private CsvFile csvFile;
 	private File file;
 	private FileReader fileReader;
 	private DataBaseInfo dbi;
@@ -21,8 +20,8 @@ public class ImportCommand{
 	
 	public ImportCommand(String[] mots) throws FileNotFoundException {
 		this.nomRelation = mots[2];
-		this.file = new File("/Users/vthar/Documents/S.csv");
-		this.fileReader = new FileReader(file);
+		this.file = new File("/users/licence/in07091/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/");
+		this.fileReader = new FileReader(file + "S.csv");
 		this.dbi = DataBaseInfo.getInstance();
 		this.indexTable = -1;
 		record = null;
@@ -31,23 +30,23 @@ public class ImportCommand{
 				indexTable = dbi.getList().indexOf(ti);
 			}
 		}
-		if(indexTable != -1) {
-			record = new Record(dbi.getList().get(indexTable));
-			
-		}
 	}
+	
 	public void execute() throws IOException {
-		try(BufferedReader  bufferReader = new BufferedReader(fileReader)){
-			String ligne;
-			String[] separateur;
-			ArrayList<Object> recvalues = new ArrayList<>();
-			while((ligne = bufferReader.readLine()) != null) {
-				separateur = ligne.split(",");
+		try(Scanner scanner = new Scanner(fileReader)){
+			while(scanner.hasNextLine()) {
+				String ligne = scanner.nextLine();
+				String[] separateur = ligne.split(",");
+				ArrayList<Object> recvalues = new ArrayList<>();
+				
 				for(int i = 0; i < separateur.length; i++) {
 					recvalues.add(separateur[i]);
 				}
+				if(indexTable != -1) {
+					record = new Record(dbi.getList().get(indexTable));
+					record.InsertValues(recvalues);	
+				}
 			}
-			record.InsertValues(recvalues);
 		}
 	}
 

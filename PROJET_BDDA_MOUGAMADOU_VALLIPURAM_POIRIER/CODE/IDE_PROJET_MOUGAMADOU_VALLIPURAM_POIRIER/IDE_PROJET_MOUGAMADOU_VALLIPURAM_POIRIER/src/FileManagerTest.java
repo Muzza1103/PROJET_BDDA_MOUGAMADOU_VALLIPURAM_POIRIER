1e6521fe82dd68e051f4178fc7c418ef453f1588
Thibhan <98709829+Thibhan@users.noreply.github.com>
2023-12-16 22:49:02 +0100

    Update FileManagerTest.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManagerTest.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManagerTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class FileManagerTest {
 
@@ -29,18 +30,41 @@ public class FileManagerTest {
         BufferManager bm = BufferManager.getInstance();
         DiskManager dk = DiskManager.getInstance();
         //System.out.println(page.toString());  
-        String nom = "Ti";
-        int colonne = 2;
-        int taille = 34;
+        ColInfo Colonne0 = new ColInfo("Nom", "VARSTRING(T)");
+        ColInfo Colonne1 = new ColInfo("Age", "INT");
+        ColInfo Colonne2 = new ColInfo("Taille", "FLOAT");
+        ColInfo Colonne3 = new ColInfo("Genre", "STRING(T)");
+
+        ArrayList<ColInfo> ListeColonnes = new ArrayList<>();
+        ListeColonnes.add(Colonne0);
+        ListeColonnes.add(Colonne1);
+        ListeColonnes.add(Colonne2);
+        ListeColonnes.add(Colonne3);
         PageId pageheader = TestcreatHeaderPage();
-        TableInfo tab = new TableInfo(nom,colonne,pageheader);
+        TableInfo tab = new TableInfo("nom-age",4,pageheader);
         PageId pageData = TestaddDataPage(tab);
         PageId pageDatab = TestaddDataPage(tab);
         System.out.println(pageData.toString());
         System.out.println(pageDatab.toString());
-		Record rec = new Record(tab);
-		//RecordId ri = TestwriteRecordToDataPage(rec,pageData);
-		PageId pagefree = TestgetFreeDataPageId(tab,taille);
+        Record Record0 = new Record(tab);
+        
+        ArrayList<Object> recValues = new ArrayList<>();
+        String nom_0 = "MonPrenom";
+        int age_0 = 20;
+        float taille_0 = 1.76f;
+        String genre_0 = "voila_20_characteres";
+
+        
+        recValues.add(nom_0);
+        recValues.add(age_0);
+        recValues.add(taille_0);
+        recValues.add(genre_0);
+        System.out.println(recValues);
+
+        Record0.InsertValues(recValues);
+
+		RecordId ri = TestwriteRecordToDataPage(Record0,pageData);
+		PageId pagefree = TestgetFreeDataPageId(tab,60);
        
         	
 	    

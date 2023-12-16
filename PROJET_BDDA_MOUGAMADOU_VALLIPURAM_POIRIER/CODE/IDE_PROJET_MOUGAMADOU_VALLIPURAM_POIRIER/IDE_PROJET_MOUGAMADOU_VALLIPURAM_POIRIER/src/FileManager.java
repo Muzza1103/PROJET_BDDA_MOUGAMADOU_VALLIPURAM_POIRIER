b2b2000ae250af0a997571e175696d78c489f4ba
Thibhan <98709829+Thibhan@users.noreply.github.com>
2023-12-16 22:48:35 +0100

    Update FileManager.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManager.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/FileManager.java
@@ -1,6 +1,7 @@
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 public class FileManager {
 	private static FileManager fm =null;
 	
@@ -98,14 +99,18 @@ public class FileManager {
 	 ByteBuffer buffparcour = BufferManager.getInstance().GetPage(page);
 	 // calcul espace libre
 	buffparcour.position(0);
+	int tailleFichier =  buffparcour.capacity(); //4096
+	//position de fin donner ecrit
 	int espacelibre = buffparcour.capacity()-4;//4096-4
-	int posDebEspaceLibre = buffparcour.getInt(espacelibre);  
-	System.out.println(posDebEspaceLibre+"posDeb");
-	int nombreslodir = buffparcour.capacity()-8;
+	int posDebEspaceLibre = buffparcour.getInt(espacelibre);
+	
+   //positon de pour nombre de record
+	int nombreslodir = buffparcour.capacity()-8;//4096-8
 	int nombreRecord = buffparcour.getInt(nombreslodir);
-	System.out.println(nombreRecord+"nbr recor");
+	//calcul pour la taille du tableau
 	int tailleTableauslot = nombreRecord *2;
-	int tailleEspacelibre = posDebEspaceLibre-tailleTableauslot;
+	//calcul Final pour l'espace libre
+	int tailleEspacelibre = tailleFichier-(tailleTableauslot+8+posDebEspaceLibre);
 	System.out.println(tailleEspacelibre+"espace libre taille");
 	BufferManager.getInstance().FreePage(page, 1);
 	if(tailleEspacelibre >=sizeRecord) {
@@ -121,18 +126,51 @@ public class FileManager {
 	}
 	public RecordId writeRecordToDataPage(Record record ,PageId pageid) {
 		ByteBuffer buff = BufferManager.getInstance().GetPage(pageid);
-        int pos  = buff.getInt((int)DBParams.SGBDPageSize);
-	    record.WriteToBuffer(buff, pos);
-	    int m = buff.getInt((int)DBParams.SGBDPageSize-4);
-	    m++;
-	    buff.putInt((int)DBParams.SGBDPageSize-4,m);
-	    if( buff.capacity() == (int)DBParams.SGBDPageSize) {
+		buff.position(0);
+		int tailleFichier =  buff.capacity(); //4096
+		//position de fin donner ecrit
+		int espacelibre = buff.capacity()-4;//4096-4
+		int posDebEspaceLibre = buff.getInt(espacelibre);
+		//ecriture du buffer
+	    record.WriteToBuffer(buff, posDebEspaceLibre);
+	   //positon de pour nombre de record
+		int nombreslodir = buff.capacity()-8;//4096-8
+		int nombreRecord = buff.getInt(nombreslodir);
+	    
+	   //on fait nombreRecord *2 pour ecrire la taille et le positionnement du record et on on se positonne
+		int posiRecordEcrireTaille = (nombreRecord * 2)-nombreslodir;
+		int tailleRecord = record.readFromBuffer(buff, posDebEspaceLibre);
+		posiRecordEcrireTaille = posiRecordEcrireTaille-tailleRecord;
+		buff.position(posiRecordEcrireTaille-4);
+		buff.putInt(posDebEspaceLibre);
+		buff.position(posiRecordEcrireTaille);
+		buff.putInt(tailleRecord);
+		
+		
+		// metre a jour positionnement espacelibre et nombre de record
+		
+		nombreRecord = nombreRecord+1;
+		buff.position(nombreslodir);
+		buff.putInt(nombreRecord);
+		
+		
+		int nouvellePositonEspaceLibre = posDebEspaceLibre+tailleRecord;
+		buff.position(espacelibre);
+		buff.putInt(nouvellePositonEspaceLibre);
+		
+		
+	    if( posDebEspaceLibre ==buff.capacity() ) {// signifie la page est pleine donc on rajoute dans la liste de fichier p
 	    	ByteBuffer buff2 = BufferManager.getInstance().GetPage(record.getTabInfoRecord().getHeaderPageId());
-	    	 if((buff2.getInt(8)==-1 )&&(buff2.getInt(12)==0)) {// correspond au cas la liste est vide
+	    	 if((buff2.getInt(8)==-1 )&&(buff2.getInt(12)==0)) {// correspond au cas la liste plein qu i ' est vide
 	 	    	// ecrire dans la headerpage   le pageId de la page 
 	 	    	buff2.putInt(8,pageid.getFileIdx());
-	 	
 	 	    	buff2.putInt(12,pageid.getPageIdx());
+	 	    	if(buff2.getInt(0)==pageid.getFileIdx()&&buff2.getInt(4)==pageid.getPageIdx()) {
+	 	    		buff.position(0);
+	 	    		buff2.position(0);
+	 	    		buff.putInt(buff2.getInt(0));
+	 	    		buff.putInt(buff2.getInt(4));
+	 	    	}
 	 	    	BufferManager.getInstance().FreePage(record.getTabInfoRecord().getHeaderPageId(), 1);
 	 	    }else {
 	 	    	int a =buff2.getInt(8);
@@ -147,8 +185,25 @@ public class FileManager {
 	    	}
 	    }
 	    BufferManager.getInstance().FreePage(pageid, 1);
-	    RecordId id = new RecordId(pageid,pos);
+	    RecordId id = new RecordId(pageid,posDebEspaceLibre);
 	    return id;
 	}
+	
+	/***
+	 * public ArrayList<Record> getRecordsInDataPage(TableInfo tabInfo,PageId pageId) {
+	 
+		// trouver la page parmis dans les deux les liste chaine
+		ByteBuffer buff = BufferManager.getInstance().GetPage(tabInfo.getHeaderPageId());
+		if(pageId.equals(tabInfo.getHeaderPageId())){
+			System.out.println("La liste de records sera vide car la page coorrspond à la headerpage");
+			return null;
+		}
+		buff.position(0);
+		if(buff.get(0)==pageId.getFileIdx() && buff.get(4)==pageId.getPageIdx()) {
+			//PageId dans la 
+		}
+		**/
+	//}
 
 	}
+	

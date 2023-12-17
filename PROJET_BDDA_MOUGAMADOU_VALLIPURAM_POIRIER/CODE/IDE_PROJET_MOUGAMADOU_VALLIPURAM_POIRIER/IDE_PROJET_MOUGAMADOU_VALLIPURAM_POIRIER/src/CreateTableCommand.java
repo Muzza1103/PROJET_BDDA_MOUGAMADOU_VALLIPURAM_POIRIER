a41b73a54e31a125186f8594dcee1fca7c0e68c9
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-12-17 21:04:00 +0100

    Update CreateTableCommand.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/CreateTableCommand.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/CreateTableCommand.java
@@ -6,27 +6,18 @@ public class CreateTableCommand {
 	private String nom; 
 	private int nbreCol;
 	private List<String> nomCol;
-	private List<TypeColonne> typeCol;
+	private List<String> typeCol;
 	
 	
 	public CreateTableCommand(String[] mots) {
 		List<String> nomColonnes = new ArrayList<>();
-		List<TypeColonne> typeColonnes = new ArrayList<>();
+		List<String> typeColonnes = new ArrayList<>();
 		String ligne = mots[3].substring(1,mots[3].length()-1);
 		String[] mots2 = ligne.split("[:,]");
 		int nbrColonne;					
 		for(int i = 0; i < mots2.length; i += 2 ) {
 			nomColonnes.add(mots2[i]);						
-			if( mots2[i+1].contains("STRING") || mots2[i+1].contains("VARSTRING") ) {
-				System.out.println(mots2[i+1]);
-				int startIndex = mots2[i+1].indexOf("(");
-				int endIndex = mots2[i+1].indexOf(")");
-				String subString = mots2[i+1].substring(startIndex + 1,endIndex);
-				typeColonnes.add(new TypeColonne(mots2[i+1],Integer.parseInt(subString)));
-			}else {
-				typeColonnes.add(new TypeColonne(mots2[i+1]));
-			
-			}
+			typeColonnes.add(mots2[i+1]);
 		}
 		nbrColonne = nomColonnes.size();
 
@@ -39,7 +30,7 @@ public class CreateTableCommand {
 	private ArrayList<ColInfo> getColInfo(){
 		ArrayList <ColInfo> colInfo = new ArrayList<>();
 		for(int i = 0; i < nomCol.size(); i++) {
-			colInfo.add(new ColInfo(nomCol.get(i), typeCol.get(i)));
+			colInfo.add(new ColInfo(nomCol.get(i), typeCol.get(i).getType()));
 		}
 		return colInfo;
 	}

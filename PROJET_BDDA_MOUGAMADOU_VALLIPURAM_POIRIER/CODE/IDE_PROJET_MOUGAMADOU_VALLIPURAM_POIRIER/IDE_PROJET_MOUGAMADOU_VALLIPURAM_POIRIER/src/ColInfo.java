5e6b917f33022e057d89f32465c81aefab3c32b6
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-12-17 20:54:42 +0100

    Update ColInfo.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/ColInfo.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/ColInfo.java
@@ -1,27 +1,28 @@
 public class ColInfo {
 
-    private String nomColonne;
-    private TypeColonne typeColonne;
-    private int t;
+    private String nom_colonne;
+    private String type_colonne;
+    private int stringSize;
     
-    public ColInfo(String nomColonne, TypeColonne typeColonne){
-        this.nomColonne = nomColonne;
-        this.typeColonne = typeColonne;
-        //this.t = t;
-        this.t = typeColonne.getOctet();
-        
+    //On utilise ce constructeur pour float et int
+    public ColInfo(String nom_colonne, String type_colonne){
+        this.nom_colonne = nom_colonne;
+        this.type_colonne = type_colonne;
+        if(type_colonne.contains("STRING")){
+            stringSize = Integer.parseInt(type_colonne.substring(type_colonne.indexOf("(")+1, type_colonne.indexOf(")")));
+            System.out.println(stringSize);
+        }
     }
 
     public String GetNomCol(){
-        return nomColonne;
+        return this.nom_colonne;
     }
 
-    public TypeColonne GetTypCol(){
-        return typeColonne;
+    public String GetTypCol(){
+        return this.type_colonne;
     }
-    
-    public int GetT() {
-    	return t;
+
+    public int getSizeString(){
+        return this.stringSize;
     }
 }
-

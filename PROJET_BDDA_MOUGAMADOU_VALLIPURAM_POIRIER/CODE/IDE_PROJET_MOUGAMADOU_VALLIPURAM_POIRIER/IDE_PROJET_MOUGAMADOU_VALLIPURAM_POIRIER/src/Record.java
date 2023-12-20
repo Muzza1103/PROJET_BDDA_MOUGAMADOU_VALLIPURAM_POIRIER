1f01db6fe6bb966a84854d107405e817251abb5a
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-12-20 12:45:58 +0100

    Update Record.java

--- PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Record.java
+++ PROJET_BDDA_MOUGAMADOU_VALLIPURAM_POIRIER/CODE/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/IDE_PROJET_MOUGAMADOU_VALLIPURAM_POIRIER/src/Record.java
@@ -1,268 +1,255 @@
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Record {
     private TableInfo tabInfo;
     private ArrayList<Object> recvalues;
     //CONSTANTE T
-    private static int T;
+    //private static int T = 20;
+
+    public Record(){
+
+    }
 
     public Record(TableInfo tabInfo){
         this.tabInfo = tabInfo;
         this.recvalues = null;
     }
 
+    public void InsertValues(ArrayList<Object> recvalues){
+        this.recvalues = recvalues;
+    }
+
     //on crée un méthode pour extraire les types de la tabinfo
     public ArrayList<String> extraireTypes(TableInfo table){
         ArrayList<String> listeTypes = new ArrayList<>();
         for(int i=0; i<table.getColInfoList().size(); i++){
-            if(!listeTypes.contains(table.getColInfo(i).GetTypCol().getType())){
-                listeTypes.add(table.getColInfo(i).GetTypCol().getType());
-            }
+            //if(!listeTypes.contains(table.getColInfo(i).GetTypCol())){
+                listeTypes.add(table.getColInfo(i).GetTypCol());
+            //}
         }
         return listeTypes;
     }
+
     
-    public void InsertValues(ArrayList<Object> recvalues){
-        this.recvalues = recvalues;
-    }
-    
+    public int WriteToBuffer(ByteBuffer buffer, int pos) {
+    int taille = pos;
 
+    ArrayList<String> types_contenus = extraireTypes(tabInfo);
+    boolean contient_var = types_contenus.stream().anyMatch(type -> type.contains("VAR"));
 
-    public int WriteToBuffer(ByteBuffer buffer, int pos){
-        int taille = pos;
-        /* 
-        if(!recvalues.isEmpty()){
-            for (Object object : recvalues) {
-                object = null;
-            }
-        }*/
-
-        //extraire la liste des types dans la table
-        ArrayList<String> types_contenus = new ArrayList<>();
-        types_contenus = extraireTypes(tabInfo);
-
-        //Ecrire recvalues dans buffer si pas de varstring
-        if(!types_contenus.contains("VARSTRING(T)")){
-            
-            //on utilise le modèle taille fixe
-            for(int i=0; i<recvalues.size(); i++){
-                //on vérifie le type de la relation
-                //gestion pour STRING
-                if(tabInfo.getColInfo(i).GetTypCol().getType().equals("STRING")){
-                    
-                    buffer.position(pos);
-
-                    buffer.put((byte)recvalues.get(i));
-
-                    pos += T;
-
-                } 
-                //gestion pour float & int
-                else {
-                    buffer.position(pos);
-                    
-                    if(tabInfo.getColInfo(i).GetTypCol().getType().equals("FLOAT")){
-                        float inter_float = (float) recvalues.get(i);
-                        buffer.put((byte)inter_float);
-                    } 
-                    
-                    else if (tabInfo.getColInfo(i).GetTypCol().getType() == "INT"){
-                        int inter_int = (int) recvalues.get(i);
-                        buffer.put((byte)inter_int);
-                    }
-                    pos += Integer.BYTES;
+    if (!contient_var) {
+        for (int i = 0; i < recvalues.size(); i++) {
+            if (tabInfo.getColInfo(i).GetTypCol().contains("STRING") && !tabInfo.getColInfo(i).GetTypCol().contains("VAR")) {
+                String valeur_string = (String) recvalues.get(i);
+                writeInBufferString(buffer, pos, valeur_string);
+                pos += tabInfo.getColInfo(i).getSizeString();
+            } else {
+                if (tabInfo.getColInfo(i).GetTypCol().equals("FLOAT")) {
+                    float inter_float = (float) recvalues.get(i);
+                    writeInBufferFloat(buffer, pos, inter_float);
+                } else if (tabInfo.getColInfo(i).GetTypCol().equals("INT")) {
+                    int inter_int = (int) recvalues.get(i);
+                    writeInBufferInt(buffer, pos, inter_int);
                 }
+                pos += Integer.BYTES;
             }
+        }
+        taille = pos - taille;
+    } else {
+        int[] tabPositions = new int[recvalues.size()];
 
-            taille = pos-taille;
+        for (int i = 0; i < recvalues.size(); i++) {
+            int T = tabInfo.getColInfo(i).getSizeString();
+
+            if (tabInfo.getColInfo(i).GetTypCol().equals("FLOAT") || tabInfo.getColInfo(i).GetTypCol().equals("INT")) {
+                tabPositions[i] = pos;
+                pos += 4;
+            } else if (tabInfo.getColInfo(i).GetTypCol().contains("STRING") && !tabInfo.getColInfo(i).GetTypCol().contains("VAR")) {
+                tabPositions[i] = pos;
+                pos += T;
+            } else if (tabInfo.getColInfo(i).GetTypCol().contains("VARSTRING")) {
+                String elementAInserer = (String) recvalues.get(i);
+                tabPositions[i] = pos;
+                pos += elementAInserer.length();
+            }
         }
 
-        else {
-            //on utilise le modèle taille variable
-            ByteBuffer nvbuff;
-            //init le offset directory
-            buffer.position(0);       
-            buffer.put((byte)(recvalues.size()+1));
-            
-            int pos_index = 1;
-            int pos_valeur = recvalues.size()+1;
+        for (int k = 0; k < recvalues.size(); k++) {
+            int positionInsertion = tabPositions[k];
 
-            int positionLastValue = 0;
-
-            int k;
-            
-            for(k=0; k<recvalues.size()-1; k++){
-                buffer.position(pos_index);
-                buffer.put((byte)pos_valeur);
-
-                if(tabInfo.getColInfo(k).GetTypCol().getType().equals("FLOAT")){
-                    float inter_float_variable = (float) recvalues.get(k);
-                    buffer.position(pos_valeur);
-                    buffer.put((byte)inter_float_variable);
-                    buffer.position(pos_index);
-                    buffer.put((byte)pos_valeur);
-                    pos_index ++;
-                    pos_valeur += Float.BYTES;
-                }
-
-                else if(tabInfo.getColInfo(k).GetTypCol().getType().equals("INT")){
-                    int inter_int_variable = (int) recvalues.get(k);
-                    buffer.position(pos_valeur);
-                    buffer.put((byte)inter_int_variable);
-                    buffer.position(pos_index);
-                    buffer.put((byte)pos_valeur);
-                    pos_index ++;
-                    pos_valeur += Integer.BYTES;
-                }
-
-                else if(tabInfo.getColInfo(k).GetTypCol().getType().equals("STRING")){
-                    buffer.position(pos_valeur);
-                    buffer.put((byte)recvalues.get(k));
-                    buffer.position(pos_index);
-                    buffer.put((byte)pos_valeur);
-                    pos_index ++;
-                    pos_valeur += T;
-                }
-
-                else if(tabInfo.getColInfo(k).GetTypCol().getType().equals("VARSTRING")){
-                    String valeur_varstring = (String) recvalues.get(k);
-                    buffer.position(pos_valeur);
-                    buffer.put((byte)recvalues.get(k));
-                    buffer.position(pos_index);
-                    buffer.put((byte)pos_valeur);
-                    pos_index ++;
-                    pos_valeur += valeur_varstring.length();
-                }
-
-                positionLastValue = k;
-
-            }
-            //dernier element
-            if(tabInfo.getColInfo(positionLastValue+1).GetTypCol().getType().equals("VARSTRING")){
-                String valeur_varstring = (String) recvalues.get(positionLastValue+1);
-                buffer.position(pos_valeur);
-                buffer.put((byte)recvalues.get(positionLastValue+1));
-                buffer.position(pos_index);
-                buffer.put((byte)pos_valeur);
-                pos_index ++;
-                buffer.position(pos_index);
-                buffer.put((byte)(positionLastValue+1+valeur_varstring.length()));
-                pos_valeur += valeur_varstring.length();
-            } else if(tabInfo.getColInfo(positionLastValue+1).GetTypCol().getType().equals("STRING")){
-                buffer.position(pos_valeur);
-                buffer.put((byte)recvalues.get(positionLastValue+1));
-                buffer.position(pos_index);
-                buffer.put((byte)pos_valeur);
-                pos_index ++;
-                buffer.position(pos_index);
-                buffer.put((byte)(positionLastValue+1+T));
-            } else if(tabInfo.getColInfo(positionLastValue+1).GetTypCol().getType().equals("INT")){
-                int inter_int_variable = (int) recvalues.get(k);
-                buffer.position(pos_valeur);
-                buffer.put((byte)inter_int_variable);
-                buffer.position(pos_index);
-                buffer.put((byte)pos_valeur);
-                pos_index ++;
-                buffer.position(pos_index);
-                buffer.put((byte)(positionLastValue+1+Integer.BYTES));
-            } else if(tabInfo.getColInfo(positionLastValue+1).GetTypCol().getType().equals("FLOAT")){
+            if (tabInfo.getColInfo(k).GetTypCol().equals("FLOAT")) {
                 float inter_float_variable = (float) recvalues.get(k);
-                buffer.position(pos_valeur);
-                buffer.put((byte)inter_float_variable);
-                buffer.position(pos_index);
-                buffer.put((byte)pos_valeur);
-                pos_index ++;
-                buffer.position(pos_index);
-                buffer.put((byte)(positionLastValue+1+Float.BYTES)); 
+                writeInBufferFloat(buffer, positionInsertion, inter_float_variable);
+            } else if (tabInfo.getColInfo(k).GetTypCol().equals("INT")) {
+                int inter_int_variable = (int) recvalues.get(k);
+                writeInBufferInt(buffer, positionInsertion, inter_int_variable);
+            } else if (tabInfo.getColInfo(k).GetTypCol().contains("STRING") && !tabInfo.getColInfo(k).GetTypCol().contains("VAR")) {
+                String valeur_string = (String) recvalues.get(k);
+                writeInBufferString(buffer, positionInsertion, valeur_string);
+            } else if (tabInfo.getColInfo(k).GetTypCol().contains("VARSTRING")) {
+                String valeur_varstring = (String) recvalues.get(k);
+                writeInBufferString(buffer, positionInsertion, valeur_varstring);
             }
-
-
-
-            taille = pos_valeur;
         }
-        
-        return taille;
+
+        taille = buffer.position() - taille;
     }
 
-    public int readFromBuffer(ByteBuffer buff, int pos){
-        
-        //extraire la liste des types dans la table
-        ArrayList<String> types_content = new ArrayList<>();
-        types_content = extraireTypes(tabInfo);
-        
-        //variables pour 
-        String intermediaire="";
-        int bufferposmove=pos;
-        int bufferposz;
-        
-        int taille=0;
-        //vider la liste de valeurs
-        if(!recvalues.isEmpty()){
-            recvalues.clear();
+    return taille;
+}
+    
+    public void writeInBufferInt(ByteBuffer buff, int pos, int aEcrire){
+        System.out.println("écrit dans le buffer:"+ aEcrire);
+        try {
+            buff.position(pos);
+            buff.putInt(aEcrire);
+        } catch (Exception e) {
+            System.out.println("Le int "+aEcrire+" a causé une erreur");
+            System.out.println(e);
+        }
+    }
+
+    public void writeInBufferFloat(ByteBuffer buff, int pos, float aEcrire){
+        System.out.println("écrit dans le buffer:"+ aEcrire);
+        try{
+            buff.position(pos);
+            buff.putFloat(aEcrire);
+        } catch (Exception e){
+            System.out.println("Le float "+aEcrire+" a causé une erreur");
+            System.out.println(e);
+        }
+    }
+
+    public void writeInBufferString(ByteBuffer buff, int pos, String aEcrire){
+        System.out.println("écrit dans le buffer:"+aEcrire);
+        try {
+            buff.position(pos);
+            byte[] tabByte = aEcrire.getBytes();
+            buff.put(tabByte);
+        } catch (Exception e) {
+            System.out.println("Le string "+aEcrire+" a causé une erreur");
+            System.out.println(e);
+        }
+    }
+
+    public int readFromBuffer(ByteBuffer buff, int pos) {
+        ArrayList<String> types_content = extraireTypes(tabInfo);
+        boolean contient_var = types_content.stream().anyMatch(type -> type.contains("VAR"));
+
+        if (recvalues == null) {
+            recvalues = new ArrayList<>();
         }
 
-        //on verifie si il y a des varstrings dans les recvalues
-        //si oui, taille variable
-        if(types_content.contains("VARSTRING(T)")){
-            for(int ite = 0; ite<tabInfo.getColInfoList().size(); ite++){
-                if(tabInfo.getColInfo(ite).GetTypCol().getType().contains("STRING")){
-                    //on récupère la valeur indiquée à la position pos
-                    bufferposz = buff.get(bufferposmove);
-                    for(int iteite = 0; iteite<T; iteite++){
-                        intermediaire += buff.get(bufferposz);
-                        bufferposz++;
-                    }
+        recvalues.clear(); // Vider la liste de valeurs
+
+        int bufferposmove = pos;
+
+        if (contient_var) {
+            for (int ite = 0; ite < tabInfo.getColInfoList().size(); ite++) {
+                if (tabInfo.getColInfo(ite).GetTypCol().contains("STRING") && !tabInfo.getColInfo(ite).GetTypCol().contains("VAR")) {
+                    int bufferposz = buff.get(bufferposmove);
+                    buff.position(bufferposz);
+                    int T = tabInfo.getColInfo(ite).getSizeString();
+                    byte[] stringBytes = new byte[T];
+                    buff.get(stringBytes);
+                    String intermediaire = new String(stringBytes);
                     recvalues.add(intermediaire);
-                    intermediaire="";
                     bufferposmove++;
-                }
-                if(tabInfo.getColInfo(ite).GetTypCol().getType().contains("INT")){
-                    bufferposz = buff.get(bufferposmove);
+                } else if (tabInfo.getColInfo(ite).GetTypCol().contains("INT")) {
+                    int bufferposz = buff.get(bufferposmove);
                     buff.position(bufferposz);
                     recvalues.add(buff.getInt());
+                    bufferposmove++;
+                } else if (tabInfo.getColInfo(ite).GetTypCol().contains("FLOAT")) {
+                    int bufferposz = buff.get(bufferposmove);
+                    buff.position(bufferposz);
+                    recvalues.add(buff.getFloat());
+                    bufferposmove++;
+                } else if (tabInfo.getColInfo(ite).GetTypCol().contains("VARSTRING")) {
+                    int bufferposz = buff.get(bufferposmove);
+                    buff.position(bufferposz);
+                    int taille_du_varstring = buff.getInt(bufferposmove + 1) - bufferposz;
+                    byte[] stringBytes = new byte[taille_du_varstring];
+                    buff.get(stringBytes);
+                    String intermediaire = new String(stringBytes);
+                    recvalues.add(intermediaire);
+                    bufferposmove++;
                 }
             }
-        } 
-        //si non, taille fixe
-        else {
-            
-            for(int m = 0; m<tabInfo.getColInfoList().size(); m++){
-                if(tabInfo.getColInfo(m).GetTypCol().getType().contains("STRING(T)")){
-                    for(int ii=0;ii<T; ii++){
-                        intermediaire += buff.get(bufferposmove);
-                        bufferposmove++;
-                    }
-                    recvalues.add(intermediaire);
-                    intermediaire="";
-                    buff.position(bufferposmove+T);
-                }
-                else if(tabInfo.getColInfo(m).GetTypCol().getType().contains("INT")){
-                    recvalues.add(buff.getInt());
-                    buff.position(bufferposmove+Integer.BYTES);
-                } else if(tabInfo.getColInfo(m).GetTypCol().getType().contains("FLOAT")){
-                    recvalues.add(buff.getFloat());
-                    buff.position(bufferposmove+Integer.BYTES);
-                } else {
-                    for(int ii=0;ii<T; ii++){
-                        intermediaire += buff.get(bufferposmove);
-                        bufferposmove++;
-                    }
-                    recvalues.add(intermediaire);
-                    intermediaire="";
-                    buff.position(bufferposmove+T);
+        } else {
+            for (int m = 0; m < tabInfo.getColInfoList().size(); m++) {
+                if (tabInfo.getColInfo(m).GetTypCol().contains("STRING(T)")) {
+                    int T = tabInfo.getColInfo(m).getSizeString();
+                    readStringFromBuffer(buff, bufferposmove, T);
+                    bufferposmove += T;
+                } else if (tabInfo.getColInfo(m).GetTypCol().contains("INT")) {
+                    readIntFromBuffer(buff, bufferposmove);
+                    bufferposmove++;
+                } else if (tabInfo.getColInfo(m).GetTypCol().contains("FLOAT")) {
+                    readFloatFromBuffer(buff, bufferposmove);
+                    bufferposmove++;
                 }
             }
         }
-        
 
-        
-
-        return taille;
+        System.out.println("recvalues: " + recvalues);
+        return bufferposmove - pos;
     }
+
+    public void readIntFromBuffer(ByteBuffer buffer, int pos) {
+        try {
+            int inter = buffer.getInt(pos);
+            recvalues.add(inter);
+            System.out.println("Int intégré à recvalues :" + inter);
+        } catch (Exception e) {
+            System.out.println("Exception caused by :" + e);
+        }
+    }
+
+    public void readFloatFromBuffer(ByteBuffer buffer, int pos) {
+        try {
+            float inter = buffer.getFloat(pos);
+            recvalues.add(inter);
+            System.out.println("Float intégré à recvalues :" + inter);
+        } catch (Exception e) {
+            System.out.println("Exception caused by :" + e);
+        }
+    }
+
+    public void readStringFromBuffer(ByteBuffer buffer, int pos, int taille) {
+        try {
+            byte[] stringBytes = new byte[taille];
+            buffer.position(pos);
+            buffer.get(stringBytes);
+            String inter = new String(stringBytes);
+            recvalues.add(inter);
+            System.out.println("Valeur intérmédiaire:" + inter);
+        } catch (Exception e) {
+            System.out.println("Exception caused by :" + e);
+        }
+    }
+
+    //récupéré sur www.java2s.com pour les tests
+    public static void printBuffer(ByteBuffer buffer) {
+    	buffer.position(0);
+    	/*
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        System.out.println((String) Arrays.toString(bytes));
+        */
+    	while (buffer.hasRemaining()) {
+            int element = buffer.getInt();
+            System.out.print(element+" ");
+        }
+    }
+
     TableInfo getTabInfoRecord() {
     	return this.tabInfo;
     }
+    
     public ArrayList<Object> getRecValues(){
     	return recvalues;
     }
